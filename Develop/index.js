f8c2ce9949bb04ddd6dc3aec9537feb6dd31635e
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-31 12:17:15 -0500

    added test question added inquire path and fs

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
-// array of questions for user
-const questions = [
+//inquirer
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMd = require('./utils/generateMarkdown');
 
-];
+// array of questions for user
+
+const questions = [
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    }
+  ];
+  
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -9,8 +21,19 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    
 }
 
 // function call to initialize program
 init();
+
+//   * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
+ 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,55 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![GitHub license](<https://img.shields.io/badge/license-${data.license}-blue.svg>)
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
 
-`;
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+
+    
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+  
+  `;
 }
 
 module.exports = generateMarkdown;
